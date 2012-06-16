1e84311cb86f8b2532191c3d2ff7d2717487e088
Kevin Moore <kevin@thinkpixellab.com>
2012-06-15 17:41:48 -0700

    Upgraded unittest code from dart source to SVN 8755
    updated tests, too

--- test/test_two_numbers.dart
+++ test/test_two_numbers.dart
@@ -4,31 +4,31 @@ class TestTwoNumbers {
     group('Two Numbers --', (){
       test('equality', () {
         var val = new TwoNumbers(1,2);
-        expect(val).equals(val);
-        expect(val).same(val);
+        expect(val, equals(val));
+        expect(val, same(val));
 
         var val2 = new TwoNumbers(7,8);
-        expect(val2 == val).isFalse();
-        expect(val2 === val).isFalse();
+        expect(val2, isNot(equals(val)));
+        expect(val2, isNot(same(val)));
 
         var val3 = new TwoNumbers(1,2);
         // values equals
-        expect(val3).equals(val);
+        expect(val3, equals(val));
         // but *not* the same object
-        expect(val3 === val).isFalse();
+        expect(val3, isNot(same(val)));
       });
-      
+
       test('default values', (){
         var val = new TwoNumbers();
-        expect(val.n1).equals(0);
-        expect(val.n2).equals(0);
+        expect(val.n1, equals(0));
+        expect(val.n2, equals(0));
 
       });
 
       test('ctor values', (){
         var val = new TwoNumbers(5,6);
-        expect(val.n1).equals(5);
-        expect(val.n2).equals(6);
+        expect(val.n1, equals(5));
+        expect(val.n2, equals(6));
       });
     });
   }
