0fe28c9311f69d23f2ae0dcf44be71ea89b07574
Luca Barbato <luca.barbato@gmail.com>
2013-12-15 17:47:30 +0100

    fix typo

--- lib/example.dart
+++ lib/example.dart
@@ -1,5 +1,5 @@
 // TODO: rename this file to match the name of your library.
-// TODO: replace 'examlpe' with the name of your library
+// TODO: replace 'example' with the name of your library
 #library('example');
 
 // TODO: rename the 'example' directory
